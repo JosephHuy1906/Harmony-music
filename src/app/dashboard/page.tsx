0ff93323f21c494f76a9103c3eabf5b01720a603
JosephHuy1906 <huydeptrai19062002@gmail.com>
2023-08-26 12:05:04 +0700

    add

--- src/app/dashboard/page.tsx
+++
@@ -1,6 +0,0 @@
-import DashboardPage from "@/core/layouts/pages/Dashboard/Dashboard.component"; 
-function DashboardRouting() {
-    return <DashboardPage></DashboardPage>;
-}
-
-export default DashboardRouting;
